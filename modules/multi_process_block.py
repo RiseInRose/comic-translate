2d048983312ed9806d9149bd18daaa79ac4a6f50
zjhmac <zjh@youjisoft.com>
2025-06-14 15:50:57 +0800

    大图片压缩后处理

--- modules/multi_process_block.py
+++ modules/multi_process_block.py
@@ -75,7 +75,9 @@ def process_block(blk, render_settings, trg_lng_cd):
     translation = blk.translation
     if not translation or len(translation) == 1:
         return None
-    width *= 1.2
+
+    if trg_lng_cd in {'zh-CN', 'zh-TW', 'zh', 'ja'}:
+        width *= 1.2
 
     translation, font_size = pyside_word_wrap(
         translation,
